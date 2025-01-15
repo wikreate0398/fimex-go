459362bd05306c57fdeb391785c83ecc939b2ba7
danielfleancu <fleancu.daniel@gmail.com>
2025-01-15 15:00:50 +0200

    ...

--- cmd/main.go
+++ cmd/main.go
@@ -3,33 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"wikreate/fimex/internal/app"
 	"wikreate/fimex/internal/config"
 	"wikreate/fimex/pkg/logger"
 )
 
-func getLogDir() (string, error) {
-	// Получение пути к исполняемому файлу
-	execPath, err := os.Executable()
-	if err != nil {
-		return "", fmt.Errorf("failed to get executable path: %w", err)
-	}
-
-	// Директория, где лежит бинарник
-	execDir := filepath.Dir(execPath)
-
-	// Путь к директории логов относительно бинарника
-	logDir := filepath.Join(execDir, "logs")
-	return logDir, nil
-}
-
 func main() {
 	log, err := logger.NewLogger()
-
-	dir, _ := getLogDir()
-
-	fmt.Println(dir)
 
 	if err != nil {
 		fmt.Println("Error initializing logger:", err)
