844cc649479b772a2982c352ee6327df2fe25c6f
danielfleancu <fleancu.daniel@gmail.com>
2025-01-17 09:23:18 +0200

    fix

--- internal/domain/interfaces/db.go
+++ internal/domain/interfaces/db.go
@@ -7,8 +7,10 @@ import (
 
 type DbManager interface {
 	GetDB() *sqlx.DB
+
 	Get(entity interface{}, query string, args ...interface{})
 	Select(entity interface{}, query string, args ...interface{})
+
 	NamedExec(query string, args interface{})
 	BatchUpdate(table string, identifier string, arg interface{}) sql.Result
 }
