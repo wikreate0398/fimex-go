815cfeeb732056918904569751f27fce90bedcff
danielfleancu <fleancu.daniel@gmail.com>
2025-02-14 13:47:34 +0200

    fix

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,14 +5,10 @@ import (
 	"fmt"
 	"go.uber.org/fx"
 	"wikreate/fimex/internal/config"
-	domain_serivces "wikreate/fimex/internal/domain/services"
+	"wikreate/fimex/internal/domain/interfaces"
 	"wikreate/fimex/internal/dto/app_dto"
-	"wikreate/fimex/internal/infrastructure/database/repositories"
-	"wikreate/fimex/internal/transport/messagebus"
-	"wikreate/fimex/internal/transport/rest"
 	"wikreate/fimex/pkg/database"
 	"wikreate/fimex/pkg/database/mysql"
-	"wikreate/fimex/pkg/lifecycle"
 	"wikreate/fimex/pkg/logger"
 )
 
@@ -20,40 +16,63 @@ func NewApplication(deps app_dto.AppDeps) *app_dto.Application {
 	return &app_dto.Application{Deps: deps}
 }
 
-func Make(cfg *config.Config, log *logger.LoggerManager) {
+func Test(logger2 interfaces.Logger) error {
+	fmt.Println("Test")
+	return nil
+}
+
+func Make() {
 	fx.New(
-		fx.Provide(logger.NewLogger),
+		fx.Provide(
+			config.NewConfig,
+			func() interfaces.Logger {
+				obj, _ := logger.NewLogger()
+				return obj
+			},
+		),
+		fx.Invoke(Test),
 		fx.Invoke(func(lc fx.Lifecycle) {
 			fmt.Println("hello")
 		}),
 	).Run()
 
 	return
-	ctx := context.Background()
+	//ctx := context.Background()
+	//
+	//dbConf := cfg.Databases.MySql
+	//dbManager := database.NewDBManager(ctx, mysql.DBCreds{
+	//	Host:     dbConf.Host,
+	//	Port:     dbConf.Port,
+	//	User:     dbConf.User,
+	//	Password: dbConf.Password,
+	//	Database: dbConf.Database,
+	//}, log)
+	//
+	//repo := repositories.NewRepositories(dbManager)
+	//serv := domain_serivces.NewServices(repo, log)
+	//
+	//app := NewApplication(app_dto.AppDeps{
+	//	Repository: repo,
+	//	Services:   serv,
+	//	Config:     cfg,
+	//	Logger:     log,
+	//})
+	//
+	//lf := lifecycle.Register(
+	//	rest.Init(app),
+	//	messagebus.Init(app),
+	//)
+	//
+	//lf.Run()
+}
 
+func ProvideMysql(ctx context.Context, cfg *config.Config, log interfaces.Logger) *database.DbAdapter {
 	dbConf := cfg.Databases.MySql
-	dbManager := database.NewDBManager(ctx, mysql.DBCreds{
+	return database.NewDBManager(ctx, mysql.DBCreds{
 		Host:     dbConf.Host,
 		Port:     dbConf.Port,
 		User:     dbConf.User,
 		Password: dbConf.Password,
 		Database: dbConf.Database,
 	}, log)
-
-	repo := repositories.NewRepositories(dbManager)
-	serv := domain_serivces.NewServices(repo, log)
-
-	app := NewApplication(app_dto.AppDeps{
-		Repository: repo,
-		Services:   serv,
-		Config:     cfg,
-		Logger:     log,
-	})
-
-	lf := lifecycle.Register(
-		rest.Init(app),
-		messagebus.Init(app),
-	)
-
-	lf.Run()
 }
